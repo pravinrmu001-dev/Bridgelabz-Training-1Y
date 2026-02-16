0a7630a3a9d64e21d3f779c26d696d74b0a7837f
Pravin <pravinrmu0812@gmail.com>
2026-02-16 22:46:39 +0530

    Assignment uploaded

---
+++ src/com/gla/Methods/Level 1/Chocolate.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Chocolate {
+
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        int eachChild = number / divisor;
+        int remaining = number % divisor;
+
+        return new int[]{eachChild, remaining};
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of chocolates: ");
+        int chocolates = sc.nextInt();
+
+        System.out.print("Enter number of children: ");
+        int children = sc.nextInt();
+
+        int[] result = findRemainderAndQuotient(chocolates, children);
+
+        System.out.println("Each child gets = " + result[0] + " chocolates");
+        System.out.println("Remaining chocolates = " + result[1]);
+
+        sc.close();
+    }
+}
